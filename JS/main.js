fe6ffc63d09fa2b4da32b7a80e8529b121f3f8a9
Ryusei <116800632+Ryusei-Hosokawa@users.noreply.github.com>
2023-02-15 22:01:43 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -56,24 +56,24 @@ window.addEventListener('scroll' , () => {
 const heroKitakan = document.querySelectorAll(".hero_kitakan");
 const heroSchool = document.querySelectorAll(".hero_school");
 const heroSapporo = document.querySelectorAll(".hero_sapporo");
-const blueBlack = '#64a4dc';
+const startBlue = '#64a4dc';
 
 gsap.set(heroKitakan,{
     y: -100,
     opacity:0,
-    color: blueBlack,
+    color: startBlue,
 });
 
 gsap.set(heroSchool,{
     y: -100,
     opacity:0,
-    color: blueBlack,
+    color: startBlue,
 });
 
 gsap.set(heroSapporo,{
     y: -100,
     opacity:0,
-    color: blueBlack,
+    color: startBlue,
 });
 
 // Processing from finish loading //
@@ -168,11 +168,11 @@ window.onload = () => {
 const mainWrap = document.querySelector(".main_wrap");
 const baseText = '#393939';
 const accentColor = '#ED9405';
+const textWhite = '#f5f3f3';
 const titleAboutH1 = document.querySelector(".title_about h1");
 const titleAboutH2 = document.querySelector(".title_about h2");
 const helloKitakan = document.querySelector("#hello_kitakan");
 const helloKitakanWrapH2 = document.querySelector(".hello_kitakan_wrap h2");
-const aboutTextMain = document.querySelector(".text_about .text_main");
 const animationTargets = document.querySelectorAll(".animation_target");
 
 // About title animation
@@ -182,91 +182,104 @@ const titleAboutPosition = titleAboutH1.getBoundingClientRect().top;
         console.log(`white`);
         mainWrap.style.cssText = `
             background: var(--base_white);
+            transition: 0.4s;
         `
         titleAboutH1.style.cssText = `
             color: var(--base_text);
+            transition: 0.4s;
         `
         titleAboutH2.style.cssText = `
             color: var(--base_text);
+            transition: 0.4s;
         `
-    } else if(titleAboutPosition <= window.innerHeight * 1.1) {
+    } else if(titleAboutPosition <= window.innerHeight * 0.95) {
         console.log(`blue`);
         mainWrap.style.cssText = `
             background: var(--base_blue);
+            transition: 0.4s;
         `
         titleAboutH1.style.cssText = `
             color: var(--text_white);
+            transition: 0.4s;
         `
         titleAboutH2.style.cssText = `
-            color: var(--text_white);
+            color: var(--base_blue);
+            transition: 0.4s;
         `
     } else {
         console.log(`black`);
         mainWrap.style.cssText = `
-            background: var(--blue_black);
+            background: var(--main_wrap_color);
         `
         titleAboutH1.style.cssText = `
             color: var(--text_white);
+            transition: 0.4s;
         `
         titleAboutH2.style.cssText = `
             color: var(--text_white);
+            transition: 0.4s;
         `
     };
 });
-        
+
 // About text animation    
 // // Element positioning and scroll animation
-const tlabout = gsap.timeline({repeat: 0, repeatDelay: 0});
-const textMain1 = document.querySelector(".text_main_1");
-const textMain2 = document.querySelector(".text_main_2");
-const textMain3 = document.querySelector(".text_main_3");
-const textMain4 = document.querySelector(".text_main_4");
-const textMain5 = document.querySelector(".text_main_5");
-const textMain6 = document.querySelector(".text_main_6");
-// about text time line animation dulation
-const aTMD = 0.1;
+const tlaboutM = gsap.timeline();
+const tlaboutS = gsap.timeline();
 
 gsap.set( animationTargets,{
     opacity: 0,
 });
 
-let tlaboutOnce = false;
+let tlAboutMOnce = false;
+let tlAboutSOnce = false;
 window.addEventListener('scroll', () => {
-const aboutTextmainPosition = aboutTextMain.getBoundingClientRect().top;
-    if(aboutTextmainPosition <= window.innerHeight * 0.7 && tlaboutOnce === false) {
+    const aboutTextMain = document.querySelector(".text_about .text_main");
+    const aTM1 = document.querySelector(".text_main_1");
+    const aTM2 = document.querySelector(".text_main_2");
+    const aTM3 = document.querySelector(".text_main_3");
+    const aTM4 = document.querySelector(".text_main_4");
+    const aTM5 = document.querySelector(".text_main_5");
+    const aTM6 = document.querySelector(".text_main_6");
+    //About text main position
+    const aTMP = aboutTextMain.getBoundingClientRect().top;
+    // about text time line animation diray
+    const aTMD = 1.2;
+
+    if(aTMP <= window.innerHeight * 0.7 && tlAboutMOnce === false) {
         console.log(`about text main gsap!!!!`);
-        tlaboutOnce = true;
-        tlabout
-        .to(textMain1, {
+        tlAboutMOnce = true;
+        tlaboutM
+        .to(aTM1, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
-        }, aTMD)
+        }, 0)
 
-        .to(textMain2, {
+        .to(aTM2, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
         }, 0.2)
 
-        .to(textMain3, {
+        .to(aTM3, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
         }, 0.4)
 
-        .to(textMain4, {
+        .to(aTM4, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
         }, 0.6)
 
-        .to(textMain5, {
+        .to(aTM5, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
         }, 0.8)
 
-        .to(textMain6, {
+        .to(aTM6, aTMD,{
             opacity: 1,
             ease: 'Power3.easeOut',
         }, 1)
-        
+        //change color orange
         .fromTo(textMain1, {
             color: baseText,
             ease: 'Power3.easeOut',
@@ -275,28 +288,195 @@ const aboutTextmainPosition = aboutTextMain.getBoundingClientRect().top;
             ease: 'Power3.easeOut',
         }, 1.4);
     };
+    
+    // About sub text animation
+    const aboutTextSub = document.querySelector(".text_about .text_sub");
+    const aTS1 = document.querySelector(".text_sub_1");
+    const aTS2 = document.querySelector(".text_sub_2");
+    const aTS3 = document.querySelector(".text_sub_3");
+    const aTS4 = document.querySelector(".text_sub_4");
+    const aTS5 = document.querySelector(".text_sub_5");
+    const aTS6 = document.querySelector(".text_sub_6");
+    const aTS7 = document.querySelector(".text_sub_7");
+    const aTS8 = document.querySelector(".text_sub_8");
+    const aTS9 = document.querySelector(".text_sub_9");
+    const aTS10 = document.querySelector(".text_sub_10");
+    //About text sub position
+    const aTSP = aboutTextSub.getBoundingClientRect().top;
+    //About text sub animation diray
+    const aTSD = 1.2;
+
+    if(aTSP <= window.innerHeight * 0.6 && tlAboutSOnce === false) {
+        console.log(`about text sub gsap!!!!`);
+        tlAboutSOnce = true;
+        tlaboutS
+        .to(aTS1, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 0)
+    
+        .to(aTS2, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 0.2)
+    
+        .to(aTS3, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 0.4)
+    
+        .to(aTS4, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 0.6)
+    
+        .to(aTS5, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 0.8)
+    
+        .to(aTS6, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 1)
+
+        .to(aTS7, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 1.2)
+
+        .to(aTS8, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 1.4)
+
+        .to(aTS9, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 1.6)
+
+        .to(aTS10, aTSD,{
+            opacity: 1,
+            ease: 'Power3.easeOut',
+        }, 1.8)
+        //change color orange
+        .fromTo(textSub9, {
+            color: textWhite,
+            ease: 'Power3.easeOut',
+        },{
+            color: accentColor,
+            ease: 'Power3.easeOut',
+        }, 2.2);
+    };
 });
 
-//about_background_blue_box animation
+//About_background_blue_box animation
 const aBBB = document.querySelector(".about_background_blue_box");
+const lessonDesk = document.querySelector(".lesson_desk");
 let aBBBOnce = false;
+
+gsap.set(lessonDesk ,{
+    x: -1000,
+    ease: 'Power4.easeOut',
+});
 
 window.addEventListener('scroll', () => {
     const aBBBPosition = aBBB.getBoundingClientRect().top;
     if(aBBBPosition <= window.innerHeight * 0.8 && aBBBOnce === false) {
         console.log(`about background animation!!!`);
         aBBBOnce = true;
+        gsap.to(lessonDesk, 0.8,{
+           x: 0,
+           ease: 'Power4.easeOut',  
+        }),
         aBBB.style.cssText = `
             width: 100vw;
-            transition: 1.2s cubic-bezier(0.135, 0.53, 0.139, 0.974);
+            transition: 1.2s 0.5s cubic-bezier(0.135, 0.53, 0.139, 0.974);
         `
     };
 });
 
+
+
+
+// Section job //
+const sectionJob = document.querySelector("#job");
+const titleJob = document.querySelector(".title_job");
+const titleJobH1 = document.querySelector(".title_job h1");
+const titleJobH2 = document.querySelector(".title_job h2");
+const textJb = document.querySelector(".title_job h2");
+const textJob = document.querySelector(".title_job h2");
+
+// Job title animation
+window.addEventListener('scroll', () => {
+    const titleJobPosition = titleJobH1.getBoundingClientRect().top;
+        if(titleJobPosition <= window.innerHeight * 0.5) {
+            console.log(`job white`);
+            sectionJob.style.cssText = `
+                background: var(--base_white);
+                transition: 0.4s;
+            `
+            titleJobH1.style.cssText = `
+                color: var(--base_text);
+                transition: 0.4s;
+            `
+            titleJobH2.style.cssText = `
+                color: var(--base_text);
+                transition: 0.4s;
+            `
+        } else {
+            console.log(`job blue`);
+            sectionJob.style.cssText = `
+                background: var(--base_blue);
+                transition: 0.4s;
+            `
+            titleJobH1.style.cssText = `
+                color: var(--text_white);
+                transition: 0.4s;
+            `
+            titleJobH2.style.cssText = `
+                color: var(--base_blue);
+                transition: 0.4s;
+            `
+        };
+
+// Job title animation
+
+    const textJobPosition = textJob.getBoundingClientRect().top;
+        if(titleJobPosition <= window.innerHeight * 0.5) {
+            console.log(`job white`);
+            sectionJob.style.cssText = `
+                background: var(--base_white);
+                transition: 0.4s;
+            `
+            titleJobH1.style.cssText = `
+                color: var(--base_text);
+                transition: 0.4s;
+            `
+            titleJobH2.style.cssText = `
+                color: var(--base_text);
+                transition: 0.4s;
+            `
+        } else {
+            console.log(`job blue`);
+            sectionJob.style.cssText = `
+                background: var(--base_blue);
+                transition: 0.4s;
+            `
+            titleJobH1.style.cssText = `
+                color: var(--text_white);
+                transition: 0.4s;
+            `
+            titleJobH2.style.cssText = `
+                color: var(--base_blue);
+                transition: 0.4s;
+            `
+        };
+    });
+    
 // Gsap ScrollTrgger//
 // Hello kitakan scroll 
 const sectionAbout = document.querySelector("#about");
-const lessonDesk = document.querySelector(".lesson_desk");
 
 gsap.to(helloKitakanWrapH2, {
     scale: 100,
@@ -312,10 +492,16 @@ gsap.to(helloKitakanWrapH2, {
 });
 
 // About animation //
+// Title animetion
 const titleAbout = document.querySelector(".title_about");
 
+gsap.set(titleAbout, {
+    scale: 0,
+    opacity: 0,
+});
+
 gsap.fromTo(titleAbout, {
-    scale: 0.2,
+    scale: 0,
     opacity: 0,
     ease: 'sine.inOut',
     },{
@@ -327,21 +513,6 @@ gsap.fromTo(titleAbout, {
         start: 'top bottom',
         end: 'top 53%',
         scrub: false,
-        markers: false,
-    }
-});
-
-gsap.fromTo(lessonDesk, {
-    x: -200,
-    ease: 'sine.inOut',
-    },{
-    x: 200,
-    ease: 'sine.inOut',
-    scrollTrigger: {
-        trigger: lessonDesk,
-        start: '100% 50%',
-        end: '100% top',
-        scrub: true,
         markers: false,
     }
 });
